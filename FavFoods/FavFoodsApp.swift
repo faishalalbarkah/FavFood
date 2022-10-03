09fd5a4b380624744611c813c0b8b720a42a0776
faisalalbarkah1414 <faisalalbarkah1414@gmail.com>
2022-10-03 14:50:54 +0700

    Initial Commit

---
+++ FavFoods/FavFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFoodsApp.swift
+//  FavFoods
+//
+//  Created by faisalalbarkah on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FavFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
